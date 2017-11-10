3fb3b5222d41f674f90dc0ecce485a34431e73fd
dongshangtong <dongshangtong@163.com>
2017-11-10 09:31:43 +0000

    add user following

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,19 +8,25 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => $user])
         </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => $user])
+        </section>
       </div>
     </div>
-
     <div class="col-md-12">
+      @if (Auth::check())
+        @include('users._follow_form')
+      @endif
+
       @if (count($statuses) > 0)
-         <ol class="statuses">
+        <ol class="statuses">
           @foreach ($statuses as $status)
-          @include('statuses._status')
+            @include('statuses._status')
           @endforeach
-       </ol>
+        </ol>
         {!! $statuses->render() !!}
-     @endif
-   </div>
+      @endif
+    </div>
   </div>
 </div>
 @stop
